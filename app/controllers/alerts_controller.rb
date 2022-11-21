30ba0c15acf6e26b83c45d2d2e85638373797cd3
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-21 10:56:10 +0300

    add ActiveAdmin

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,39 @@
+class AlertsController < ApplicationController
+  before_action :set_service, only: %i[destroy]
+  # before_action :get_users
+
+  def index
+    @alerts = Alert.all
+  end
+
+  def new
+    @alert = Alert.new
+  end
+
+  def create
+    @alert = Alert.new(alert_params)
+    return unless @alert.save
+
+    @users.each do |user|
+      AlertMailer.with(alert: @alert).alert_posted_email(user).deliver_later
+    end
+  end
+
+  def destroy
+    @alert.destroy
+  end
+
+  # def get_users
+  #   @users = User.all
+  # end
+
+  private
+
+  def set_alert
+    @alert = alert.find(params[:id])
+  end
+
+  def alert_params
+    params.require(:alert).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  resources :users
+  resources :alerts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "alerts#index"
 end
